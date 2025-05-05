77d5c8db8f222f8dec0c4cd88eecfaae3c033c4e
Markus Wüstenberg <markus@maragu.dk>
2025-05-05 11:56:15 +0200

    Add workaround for Anthropic SDK JSON error in streaming

--- chat_complete_test.go
+++ chat_complete_test.go
@@ -126,6 +126,61 @@ func TestChatCompleter_ChatComplete(t *testing.T) {
 
 		is.Equal(t, `The readme.txt file simply contains the text "Hi!" - it's a very brief readme file.`, output)
 	})
+
+	t.Run("can use a tool with no args", func(t *testing.T) {
+		cc := newChatCompleter(t)
+
+		req := gai.ChatCompleteRequest{
+			Messages: []gai.Message{
+				gai.NewUserTextMessage("What is in the current directory?"),
+			},
+			Temperature: gai.Ptr(gai.Temperature(0)),
+			Tools: []gai.Tool{
+				tools.NewListDir(testdata),
+			},
+		}
+
+		res, err := cc.ChatComplete(t.Context(), req)
+		is.NotError(t, err)
+
+		var output string
+		var found bool
+		var parts []gai.MessagePart
+		var result gai.ToolResult
+		for part, err := range res.Parts() {
+			is.NotError(t, err)
+
+			parts = append(parts, part)
+
+			switch part.Type {
+			case gai.MessagePartTypeToolCall:
+				toolCall := part.ToolCall()
+				for _, tool := range req.Tools {
+					if tool.Name == toolCall.Name {
+						found = true
+						content, err := tool.Function(t.Context(), toolCall.Args)
+						result = gai.ToolResult{
+							ID:      toolCall.ID,
+							Content: content,
+							Err:     err,
+						}
+						break
+					}
+				}
+
+			case gai.MessagePartTypeText:
+				output += part.Text()
+
+			default:
+				t.Fatal("unexpected message parts")
+			}
+		}
+
+		is.Equal(t, "I'll help you list the contents of the current directory. I'll use the `list_dir` function to show you what files and directories are present.", output)
+		is.True(t, found, "tool not found")
+		is.Equal(t, `["testdata/","testdata/readme.txt"]`, result.Content)
+		is.NotError(t, result.Err)
+	})
 }
 
 func newChatCompleter(t *testing.T) *anthropic.ChatCompleter {
